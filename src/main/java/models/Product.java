08055e73c81994ce8d849d1c46a1ead97871cd92
luizaalanis <luizalanisantos@gmail.com>
2023-01-15 23:05:42 -0300

    creating class atributes

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,9 +1,15 @@
 package models;
 // TODO: Fix imports
+import java.io.Serializable;
+import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name= "TB_PRODUCT")
-public class Product {
+public class Product implements Serializable{
+    private long id;
+    private String name;
+    private BigDecimal quantity;
+    private BigDecimal price;
 }
