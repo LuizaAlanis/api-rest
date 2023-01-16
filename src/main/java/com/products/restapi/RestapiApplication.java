33bdb64e166617fe5b02f024f2851d53f8eb07d1
luizaalanis <luizalanisantos@gmail.com>
2023-01-15 23:03:31 -0300

    creating model

--- src/main/java/com/products/restapi/RestapiApplication.java
+++ src/main/java/com/products/restapi/RestapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestapiApplication {
-
+// Obs.: This class needs to be on project root to work, cause it's a start class.
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
---
+++ src/main/java/models/Product.java
@@ -0,0 +1,9 @@
+package models;
+// TODO: Fix imports
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "TB_PRODUCT")
+public class Product {
+}
