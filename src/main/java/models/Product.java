ae666f7c5483eb19c4848b130bb1f0a8576d4d06
luizaalanis <luizalanisantos@gmail.com>
2023-01-15 23:07:53 -0300

    spring annotations

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,13 +1,20 @@
 package models;
-// TODO: Fix imports
+
 import java.io.Serializable;
 import java.math.BigDecimal;
+
+// TODO: Fix imports
+import javax.annotation.processing.Generated;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name= "TB_PRODUCT")
 public class Product implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private BigDecimal quantity;
