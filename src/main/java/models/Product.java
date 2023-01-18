541294c7c29aa328cf93009efca271296b403e60
luizaalanis <luizalanisantos@gmail.com>
2023-01-17 23:32:31 -0300

    generate getters and setters

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -19,4 +19,36 @@ public class Product implements Serializable{
     private String name;
     private BigDecimal quantity;
     private BigDecimal price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
